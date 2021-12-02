00acfe0990b962d4890dbef9258a48c2027a9762
iammursal <iammursal@gmail.com>
2021-12-02 22:14:14 +0530

    Google UX/UI certificatie added

--- js/main.js
+++
@@ -1,8 +0,0 @@
-/*! MurshalAkhtar v0.0.1 | (c) 2021 Murshal Akhtar Ansari | MIT License | http://link-to-your-git-repo.com */
-/**
- * Element.matches() polyfill (simple version)
- * https://developer.mozilla.org/en-US/docs/Web/API/Element/matches#Polyfill
- */
-if (!Element.prototype.matches) {
-	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
-}
