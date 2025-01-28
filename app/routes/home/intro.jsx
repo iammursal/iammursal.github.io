d05238da813d9b9715de82d6a837b6455940fe19
Murshal Akhtar <iammursal@gmail.com>
2025-01-28 17:03:50 +0300

    feat: add Hyperspeed component and remove DisplacementSphere; update styles and dependencies

--- app/routes/home/intro.jsx
+++ app/routes/home/intro.jsx
@@ -12,11 +12,90 @@ import { VisuallyHidden } from '~/components/visually-hidden';
 import config from '~/config.json';
 import { useInterval, usePrevious, useScrollToHash } from '~/hooks';
 import { useHydrated } from '~/hooks/useHydrated';
+import Hyperspeed from './Hyperspeed/Hyperspeed';
 import styles from './intro.module.css';
 
-const DisplacementSphere = lazy(() =>
-    import('./displacement-sphere').then(module => ({ default: module.DisplacementSphere }))
-);
+
+// the component will fill the height/width of its parent container, edit the CSS to change this
+// the options below are the default values
+const DisplacementSphere = () => <Hyperspeed
+    effectOptions={{
+        onSpeedUp: () => { },
+
+        onSlowDown: () => { },
+
+        distortion: 'turbulentDistortion',
+
+        length: 400,
+
+        roadWidth: 8,
+
+        islandWidth: 2,
+
+        lanesPerRoad: 4,
+
+        fov: 90,
+
+        fovSpeedUp: 150,
+
+        speedUp: 2,
+
+        carLightsFade: 0.4,
+
+        totalSideLightSticks: 20,
+
+        lightPairsPerRoadWay: 40,
+
+        shoulderLinesWidthPercentage: 0.05,
+
+        brokenLinesWidthPercentage: 0.1,
+
+        brokenLinesLengthPercentage: 0.5,
+
+        lightStickWidth: [0.12, 0.5],
+
+        lightStickHeight: [1.3, 1.7],
+
+        movingAwaySpeed: [60, 80],
+
+        movingCloserSpeed: [-120, -160],
+
+        carLightsLength: [400 * 0.03, 400 * 0.2],
+
+        carLightsRadius: [0.05, 0.14],
+
+        carWidthPercentage: [0.3, 0.5],
+
+        carShiftX: [-0.8, 0.8],
+
+        carFloorSeparation: [0, 5],
+
+        colors: {
+
+            roadColor: 0x111111,
+
+            islandColor: 0x111111,
+
+            background: 0x000000,
+
+            shoulderLines: 0xFFFFFF,
+
+            brokenLines: 0xFFFFFF,
+
+            leftCars: [0xD856BF, 0x6750A2, 0xC247AC],
+
+            rightCars: [0x03B3C3, 0x0E5EA5, 0x324555],
+
+            sticks: 0x03B3C3,
+
+        }
+
+    }}
+
+/>
+// const DisplacementSphere = lazy(() =>
+//     import('./displacement-sphere').then(module => ({ default: module.DisplacementSphere }))
+// );
 
 export function Intro({ id, sectionRef, scrollIndicatorHidden, ...rest }) {
     const { theme } = useTheme();
